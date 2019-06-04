237e14a5ad19b0d8d5d1c71cd36c470bcc71f01e
zeratwo2 <zeratwo@gmail.com>
2019-06-04 13:02:21 +0300

    exercise 1.1

---
+++ hello.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var rub float64
+	var dollar float64
+	//const dollar float64 = 65.17
+
+	dollar = 65.17
+
+	fmt.Println("vvedite summu rublei:")
+	fmt.Scanln(&rub)
+	fmt.Println("otvet:", rub/dollar)
+
+}
+
+func add() {
+	var ABC float64
+	katet1 := 4
+	katet2 := 6
+}
